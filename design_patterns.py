2a0cce1b8f625069c0b4eac6bca87eaf42a08d80
AI2Enginex <vibhorsakalley@gmail.com>
2025-04-19 23:11:05 +0530

    updated file with string and list examples

--- design_patterns.py
+++ design_patterns.py
@@ -1,14 +1,15 @@
 # Import TypedDict from typing_extensions.
 # TypedDict allows us to specify the expected structure of a dictionary with fixed keys and their types.
-from typing_extensions import TypedDict
+from typing_extensions import TypedDict,List,Optional
 
 # Define a custom type 'Variables' using TypedDict.
 # This type ensures that any dictionary passed to the functions must contain two integer keys: 'num1' and 'num2'.
-class Variables(TypedDict):
+class Variables(TypedDict, total=False):
     num1: int  # First number (integer)
     num2: int  # Second number (integer)
     var: str   # string input
-    sequence: list # list input
+    sequence: Optional[List[int]] = None # list input
+    str_sequence: Optional[List[str]] = None  #list of strings
 
 # Define a class that performs basic arithmetic operations.
 class ArathematicOperations:
@@ -62,12 +63,71 @@ class ArathematicOperations:
 # Placeholder class for string-related operations.
 # Currently not implemented but defined for future expansion of the program.
 class StringOperations:
-    pass
+
+    def __init__(self):
+        pass
+
+    # Method for converting string
+    # to lowercase using pydantic
+    def make_lower(self,parameter: Variables):
+
+        try:
+            return parameter['var'].lower()
+        except Exception as e:
+            return e
+    
+    # Method for converting string
+    # to uppercase using pydantic
+    def make_upper(self,parameter: Variables):
+
+        try:
+            return parameter['var'].upper()
+        except Exception as e:
+            return e
+    
+    # Method for converting first character 
+    # uppercase using pydantic of string to
+    def make_capitalize(self,parameter: Variables):
+
+        try:
+            return parameter['var'].capitalize()
+        except Exception as e:
+            return e
+        
+
+    # Method for splitting a string using a delimeter
+    # and returning a list using pydantic module
+    def split_characters(self,parameter: Variables,delimiter: None):
+
+        try:
+            return parameter['var'].split(delimiter)
+        except Exception as e:
+            return e
 
 # Placeholder class for list-related operations.
 # Like StringOperations, it's a stub for future methods that may handle lists.
 class ListOperations:
-    pass
+
+    def __init__(self):
+        pass
+
+    # Method for implementing list 
+    # comprehension using TypeDict
+    def list_comprehension(self, parameter: Variables):
+
+        try:
+            return [data ** 2 for data in parameter['sequence']]
+        except Exception as e:
+            return e
+    
+    # Method for implementing list 
+    # of string operations using TypeDict
+    def list_of_strings(self, parameter: Variables):
+
+        try:
+            return [data.lower() for data in parameter['str_sequence']]
+        except Exception as e:
+            return e
 
 # This is the main entry point of the script.
 # Code inside this block only runs when the script is executed directly.
@@ -85,3 +145,24 @@ if __name__ == '__main__':
 
     # Print the result of the multiplication.
     print(multiplication_result)
+
+    ls = ListOperations()
+
+    lst_result = ls.list_comprehension({'sequence': [1,2,3,4,5]})
+
+    print(lst_result)
+
+
+    str_list_ops = ls.list_of_strings({'str_sequence': ["HELLO","HII","HOW"]})
+    print(str_list_ops)
+    
+
+    st = StringOperations()
+
+    capitalize_result = st.make_capitalize({'var': "mumbai indians"})
+
+    print(capitalize_result)
+
+    split_result = st.split_characters({'var': "mumbai indians"},delimiter=" ")
+
+    print(split_result)
